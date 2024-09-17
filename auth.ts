8b2391ee146e2a3f46ed31119e916414f1b08d1d
Tomer Fishaimer <tomer.fishaimer@aquasec.com>
2024-09-17 14:37:53 +0300

    feat: Add Google as a provider for NextAuth authentication

--- auth.ts
+++ auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { db } from "./app/db/drizzle";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  providers: [GitHub, Google],
   adapter: DrizzleAdapter(db),
   pages: {
     signIn: "/sign-in",
