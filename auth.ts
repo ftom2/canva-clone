40175bc0d2ae60e77a094d3c2134f57a60801b7e
Tomer Fishaimer <tomer.fishaimer@aquasec.com>
2024-09-17 11:56:43 +0300

    feat: Add drizzle integration

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { db } from "./app/db/drizzle";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  adapter: DrizzleAdapter(db),
 });
